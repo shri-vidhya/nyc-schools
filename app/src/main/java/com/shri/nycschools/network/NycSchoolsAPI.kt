42f54d18c9e330ce47137b2a2ea745ca6c599d49
Shri Vidhya <shrivid94@gmail.com>
2022-12-11 13:16:17 -0800

    Adding retrofit and defining APIs

---
+++ app/src/main/java/com/shri/nycschools/network/NycSchoolsAPI.kt
@@ -0,0 +1,44 @@
+package com.shri.nycschools.network
+
+import com.google.gson.GsonBuilder
+import com.shri.nycschools.model.HighSchoolDTO
+import com.shri.nycschools.model.SATScoresDTO
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+open class NycSchoolsAPI {
+    interface Api {
+        /**
+         * Get list of High Schools in NYC
+         */
+        @GET("/resource/s3k6-pzi2.json")
+        suspend fun getNycHighSchoolList(): Response<List<HighSchoolDTO>>
+
+        /**
+         * Get SAT Scores of the school
+         * @param dbn - The unique dbn number of the school
+         */
+        @GET("/resource/f9bf-2cp4.json")
+        suspend fun getSATScores(dbn: String): Response<SATScoresDTO>
+    }
+
+    open suspend fun getNycHighSchoolList(): Response<List<HighSchoolDTO>> {
+        return webservice().getNycHighSchoolList()
+    }
+    open suspend fun getSATScores(): Response<SATScoresDTO> {
+        return webservice().getSATScores()
+    }
+
+    companion object {
+        private const val BASE_URL = "https://data.cityofnewyork.us"
+
+        private fun webservice(): Api {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build().create(Api::class.java)
+        }
+    }
+}
