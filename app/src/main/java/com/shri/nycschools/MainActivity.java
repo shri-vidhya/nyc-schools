de5d874f52774e49f32b2934bbce5ab83352e42d
Shri Vidhya <shrivid94@gmail.com>
2022-12-10 22:56:44 -0800

    Creating basic application with simple navigation host and ViewModel

---
+++ app/src/main/java/com/shri/nycschools/MainActivity.java
@@ -0,0 +1,27 @@
+package com.shri.nycschools;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.nycschools.R;
+import com.example.nycschools.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController.navigate(R.id.navigation_home);
+    }
+
+}
